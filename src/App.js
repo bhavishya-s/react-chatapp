c47660802613db90fd552584ac9c8751e190e309
InvictusTTV <44098372+InvictusTTV@users.noreply.github.com>
2020-08-30 18:37:22 +0530

    auth works for switching pages now
    Co-Authored-By: A7GoD <60264425+a7god@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import { auth } from "./firebase/firebase.utils.js";
+
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ChatPage from "./pages/chatpage/chatpage.component.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/chat" component={ChatPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    const { currentUser } = this.state;
+    console.log(currentUser);
+    return (
+      <div className="App">
+        <Switch>
+          {currentUser == null ? (
+            <Route exact path="/" component={HomePage} />
+          ) : (
+            <Route exact path="/" component={ChatPage} />
+          )}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
