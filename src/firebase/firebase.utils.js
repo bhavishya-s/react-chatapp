6c9e9b2b3a3caae182ab89ca0d8023f3fe9500d4
InvictusTTV <44098372+InvictusTTV@users.noreply.github.com>
2020-08-30 17:52:45 +0530

    put config in a seperate file and added to .gitignore
    Co-Authored-By: A7GoD <60264425+a7god@users.noreply.github.com>

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const config = {
-  apiKey: "AIzaSyBg3S78Wxa-E5ANqKnzUCF_qiJrVzYDdjM",
-  authDomain: "chat-app-699b2.firebaseapp.com",
-  databaseURL: "https://chat-app-699b2.firebaseio.com",
-  projectId: "chat-app-699b2",
-  storageBucket: "chat-app-699b2.appspot.com",
-  messagingSenderId: "9891747269",
-  appId: "1:9891747269:web:ea3824e43c77c4ab2d27ac",
-  measurementId: "G-4G8455K4VT",
-};
+import config from "../../config.js";
 
 firebase.initializeApp(config);
 
